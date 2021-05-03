964bfc7de4c1947414f16c59d50b832ad0077df0
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-05-03 00:29:18 -0400

    virtual addresses --> physical addresses

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 #### Author: Gabriel Hofer
 #### Date: May 2, 2021
 
-
-
+## TODO
+1. print formatted memory map to stdin
+2. write tests for code
 
 
--- dash.c
+++ dash.c
@@ -1,14 +1,66 @@
 #include <stdio.h>
 
+/********************************************//**
+ *  p is the number of bits in a page
+ ***********************************************/
+const int p = 12;
+
+/********************************************//**
+ * @param R - resident bit
+ * @param D - dirty bit
+ * @param PPN - physical page number
+ ***********************************************/
+struct MMU {
+
+  char R[16];
+  char D[16];
+  char PPN[16];
+
+
+  /********************************************//**
+   *  handle a missing page
+   ***********************************************/
+  void PageFault(int VPageNo){
+    int i;
+
+    i = SelectLRUPage();
+    if(D[i] == 1)
+      WritePage(DiskAdr[i],PPN[i]);
+    R[i] = 0;
+
+    PPN[VPageNo] = PPN[i];
+    ReadPage(DiskAdr[VPageNo], PPN[i]);
+    R[VPageNo] = 1;
+    D[VPageNo] = 1;
+  }
+
+  /********************************************//**
+   * @return physical address
+   ***********************************************/
+  int VtoP(int Vaddr){
+    int VPageNo = Vaddr >> p;
+    int PO = Vaddr & ((1 << p) - 1);
+
+    if(R[VPageNo] == 0)
+      PageFault(VPageNo);
+    return (PPN[VPageNo] << p) | PO;
+  }
+
+
+
+};
+
+
 void REPL(){
-	char * buf=NULL;
-	size_t leng=64;
-	for(;;){
-		getline(&buf, &leng, stdin);
-		printf("%s", buf);
-	}
+  char * buf=NULL;
+  size_t leng=64;
+  for(;;){
+    getline(&buf, &leng, stdin);
+    printf("%s", buf);
+  }
 }
 
 void main(){
-	REPL();
+  REPL();
 }
+
