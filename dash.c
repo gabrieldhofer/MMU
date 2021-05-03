0f78d08635d3e97eaa643420662b0d2865c32684
Gabriel Hofer <leirbagabriel468@gmail.com>
2021-05-03 00:36:16 -0400

    it compiles now...

--- dash.c
+++ dash.c
@@ -10,45 +10,47 @@ const int p = 12;
  * @param D - dirty bit
  * @param PPN - physical page number
  ***********************************************/
-struct MMU {
 
-  char R[16];
-  char D[16];
-  char PPN[16];
+char R[16];
+char D[16];
+char PPN[16];
 
-
-  /********************************************//**
-   *  handle a missing page
-   ***********************************************/
-  void PageFault(int VPageNo){
-    int i;
-
-    i = SelectLRUPage();
-    if(D[i] == 1)
-      WritePage(DiskAdr[i],PPN[i]);
-    R[i] = 0;
-
-    PPN[VPageNo] = PPN[i];
-    ReadPage(DiskAdr[VPageNo], PPN[i]);
-    R[VPageNo] = 1;
-    D[VPageNo] = 1;
-  }
-
-  /********************************************//**
-   * @return physical address
-   ***********************************************/
-  int VtoP(int Vaddr){
-    int VPageNo = Vaddr >> p;
-    int PO = Vaddr & ((1 << p) - 1);
-
-    if(R[VPageNo] == 0)
-      PageFault(VPageNo);
-    return (PPN[VPageNo] << p) | PO;
-  }
+int SelectLRUPage(){ }
+void ReadPage(int a, int b){ }
+void WritePage(int a, int b){ }
+int DiskAdr[16];
 
 
 
-};
+/********************************************//**
+ * Handle a missing page
+ ***********************************************/
+void PageFault(int VPageNo){
+  int i;
+
+  i = SelectLRUPage();
+  if(D[i] == 1)
+    WritePage(DiskAdr[i],PPN[i]);
+  R[i] = 0;
+
+  PPN[VPageNo] = PPN[i];
+  ReadPage(DiskAdr[VPageNo], PPN[i]);
+  R[VPageNo] = 1;
+  D[VPageNo] = 1;
+}
+
+/********************************************//**
+ * @return physical address
+ ***********************************************/
+int VtoP(int Vaddr){
+  int VPageNo = Vaddr >> p;
+  int PO = Vaddr & ((1 << p) - 1);
+
+  if(R[VPageNo] == 0)
+    PageFault(VPageNo);
+  return (PPN[VPageNo] << p) | PO;
+}
+
 
 
 void REPL(){
